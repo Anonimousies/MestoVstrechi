fb8cc50ad65b403c9cf9ac5c79df060e987934b0
Anonimousies <81581745+Anonimousies@users.noreply.github.com>
2024-04-21 00:28:04 +0300

    Add files via upload

--- osnova.py
+++ osnova.py
@@ -1,19 +1,30 @@
 from flask import Flask,render_template, request
+import sqlite3
 from geopy.geocoders import Nominatim
+
+
+
 app = Flask(__name__)
 
 @app.route('/',  methods=['GET', 'POST'])
 def render_map():
-    a = ['Невский проспект', 'метро Петроградская', 'Дворцовая площадь']
-  #  if request.method == 'POST':
-  #      user_name = request.form['name']
-    geolocator = Nominatim(user_agent="coordinates_finder")
-    b = []
-    for i in a:
-        location = geolocator.geocode(i)
-        c = [location.latitude, location.longitude]
-        b.append(c)
-    return render_template('map.html', b = b)	
+    try:
+        if request.method == 'POST':
+            user_name = request.form['name']
+            connection = sqlite3.connect('my_database.db')
+            cursor = connection.cursor()
+            cursor.execute('SELECT username, age FROM Users WHERE username = ?', (user_name, ))
+            results = cursor.fetchall()
+            geolocator = Nominatim(user_agent="coordinates_finder")
+
+            a = []
+            for i in results:
+                location = geolocator.geocode(i[1])
+                a.append([location.latitude, location.longitude])
+        return render_template('map.html', b = a)
+    except:
+        return render_template('map.html')
 	
 if __name__ == '__main__':
     app.run(debug = True)
+connection.close()
