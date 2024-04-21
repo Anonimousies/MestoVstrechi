5df14f4209f0a47e7c79ed52bb42d7b73507b684
Anonimousies <81581745+Anonimousies@users.noreply.github.com>
2024-04-21 04:19:47 +0300

    Add files via upload

--- osnova.py
+++ osnova.py
@@ -8,22 +8,20 @@ app = Flask(__name__)
 
 @app.route('/',  methods=['GET', 'POST'])
 def render_map():
-    try:
-        if request.method == 'POST':
-            user_name = request.form['name']
-            connection = sqlite3.connect('my_database.db')
-            cursor = connection.cursor()
-            cursor.execute('SELECT username, age FROM Users WHERE username = ?', (user_name, ))
-            results = cursor.fetchall()
-            geolocator = Nominatim(user_agent="coordinates_finder")
-
-            a = []
-            for i in results:
-                location = geolocator.geocode(i[1])
-                a.append([location.latitude, location.longitude])
-        return render_template('map.html', b = a)
-    except:
-        return render_template('map.html')
+    user_name=''
+    if request.method == 'POST':
+        user_name = request.form['name']
+    connection = sqlite3.connect('my_database.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT username, event, descriptions, howpeople, illustration, location FROM Users WHERE event = ?', (user_name, ))
+    results = cursor.fetchall()
+    geolocator = Nominatim(user_agent="coordinates_finder")
+    a =[]
+    for i in range(len(results)):
+        location = geolocator.geocode(results[i][5])
+        a.append([results[i][0:5], [location.latitude, location.longitude]])
+    return render_template('map.html', b = a)
+    
 	
 if __name__ == '__main__':
     app.run(debug = True)
