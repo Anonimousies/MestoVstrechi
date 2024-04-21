e396c12f33f635c2def916474bf619648a9f5aa2
Anonimousies <81581745+Anonimousies@users.noreply.github.com>
2024-04-21 15:27:48 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -18,7 +18,7 @@ illustration TEXT NOT NULL
 ''')
 
 #cursor.execute('INSERT INTO Users (username, event, location, descriptions, howpeople, illustration) VALUES (?, ?, ?, ?, ?, ?)', ('Александр Рубайло', 'шахматы', 'Санкт Петербург Невский проспект дом 10', 'приглашаю приятного собеседника на партию', 1, 'https://cdn.culture.ru/images/c1b73537-b2dd-52cf-8682-2fea8c2410b6'))
-cursor.execute('INSERT INTO Users (username, event, location, descriptions, howpeople, illustration) VALUES (?, ?, ?, ?, ?, ?)', ('Василий Райенович', 'нарды', 'Метро Проспект провещения', 'Раскинем пару раз вечерком', 5, 'https://content1.rozetka.com.ua/goods/images/original/254075415.jpg'))
+cursor.execute('INSERT INTO Users (username, event, location, descriptions, howpeople, illustration) VALUES (?, ?, ?, ?, ?, ?)', ('Ян Непомнящий', 'шахматы', 'Метро Дыбенко', 'У меня Карлсон пощады просил', 12, 'https://www.bon-cadeau.ru/mdata/gallery/size3/1184/4577.jpg'))
 
 # Сохраняем изменения и закрываем соединение
 
